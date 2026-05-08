2819f6e278b5093b04f1f2a1f667724fb6e5ac22
Om Tailor <omtailork@gmail.com>
2026-05-08 11:13:19 +0530

    Removed unused import libs

--- backend/grocery/views.py
+++ backend/grocery/views.py
@@ -1,5 +1,4 @@
 from datetime import date, timedelta, datetime
-from httpx import request
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
