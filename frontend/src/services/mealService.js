675ad2d34fab841041a0f3f810c8d6f66599a342
Om Tailor <omtailork@gmail.com>
2026-05-16 19:33:56 +0530

    increased time on frontend so plan generation dosent fail

--- frontend/src/services/mealService.js
+++ frontend/src/services/mealService.js
@@ -3,9 +3,9 @@ import API from './api'
 export const mealService = {
   getWeeklyPlan: () => API.get('/meals/weekly/'),
   getDayMeal: (date, opts) => API.get(`/meals/day/${date}/`, opts),
-  generatePlan: () => API.post('/meals/generate/'),
+  generatePlan: () => API.post('/meals/generate/', null, { timeout: 180000 }),
   regenerateDay: (date) => API.post('/meals/regenerate-day/', { date }),
-  generateNextWeek: () => API.post('/meals/generate-next-week/'),
+  generateNextWeek: () => API.post('/meals/generate-next-week/', null, { timeout: 180000 }),
   getLatestPlan: () => API.get('/meals/latest/'),
   getBatchDayMeals: (dates, opts) => API.get(`/meals/batch/?dates=${dates.join(',')}`, opts),
 }
